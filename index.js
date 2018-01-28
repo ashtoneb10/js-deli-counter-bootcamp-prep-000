4ec9785b857bf2f85981cf562bdd8645db8d0e78
Ashton Blair <ashtoneb10@aol.com>
2018-01-28 02:02:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli= [];
 
 function takeANumber(katzDeliLine, name){
   katzDeli.push(name);
-  var place = katzDeli.length + 1;
+  var place = katzDeli.length;
   return `Welcome, ${name}. You are number ${place} in line.`;
 }
