bc341b82fd449700a70015235e80a494e5f78970
Ashton Blair <ashtoneb10@aol.com>
2018-01-28 02:19:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeliLine){
 function currentLine(katzDeliLine){
   if(katzDeliLine.length >0){
     for(let i = 0; i<katzDeliLine.length; i++){
-    return `The line is currently: ${i+ 1}. ${katzDeliLine[i]}`
+    array.push(`${i+ 1}. ${katzDeliLine[i]}`);
     }
 } else return "The line is currently empty."
 }
