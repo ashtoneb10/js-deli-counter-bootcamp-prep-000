7a49a172c206dd2f219b2fcc40782511dd4a01bb
Ashton Blair <ashtoneb10@aol.com>
2018-01-28 02:11:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(){
-  if(katzDeliLine.length >0){
+  if(katzDeliLine.length <0){
     return katzDeliLine.shift();
 } else return "There is nobody waiting to be served!"
 }
